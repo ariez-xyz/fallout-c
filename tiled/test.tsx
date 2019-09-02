3bc79da37b8d384912fdec0a0c08b3349ddb586f
Ariez <dpape39@icloud.com>
2019-09-02 19:06:29 +0200

    update tilesets/overworld etc (still just placeholders)

--- tiled/test.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="test" tilewidth="8" tileheight="8" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="8" height="8" source="../assets/gridbase.png"/>
- </tile>
-</tileset>
---
+++ tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="spritesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="1024" columns="32">
+ <image source="src/assets/spritesheet.png" width="543" height="543"/>
+</tileset>
